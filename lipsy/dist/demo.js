57925b3fa8fa9e6394a4acc18e7f77e17fcf2ab7
Niloy Mondal <niloy.mondal84@gmail.com>
2015-12-16 23:34:39 +0530

    firefox compatibility

--- lipsy/dist/demo.js
+++ lipsy/dist/demo.js
@@ -1,12 +1,12 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-const R = require("ramda");
-const parse = require("./parser");
-const evaluate = require("./evaluator");
+var R = require("ramda");
+var parse = require("./parser");
+var evaluate = require("./evaluator");
 
-const removeComments = R.replace(/;.*/g, "");
-const getElement = document.getElementById.bind(document);
-const fetchCode = num => getElement("codebox" + num).value;
-const runButtonClicked = R.pipe(fetchCode, removeComments, str => "[" + str + "]", parse, evaluate.bind(null, {}), result => console.log(R.last(result)));
+var removeComments = R.replace(/;.*/g, "");
+var getElement = document.getElementById.bind(document);
+var fetchCode = num => getElement("codebox" + num).value;
+var runButtonClicked = R.pipe(fetchCode, removeComments, str => "[" + str + "]", parse, evaluate.bind(null, {}), result => console.log(R.last(result)));
 
 window.onload = function () {
   R.range(1, 9).forEach(function (n) {
@@ -19,11 +19,11 @@ window.onload = function () {
 
 var _slicedToArray = (function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; })();
 
-function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) arr2[i] = arr[i]; return arr2; } else { return Array.from(arr); } }
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
 
-const R = require("ramda");
+var R = require("ramda");
 
-const symbolTable = {
+var symbolTable = {
   "print": function (_) {
     var _console;
 
@@ -84,7 +84,7 @@ const symbolTable = {
   "filter": (symTbl, fn, list) => R.filter($ => invokeFunction(symTbl, fn, [$]), list),
   "reduce": (symTbl, fn, seed, list) => R.reduce((a, b) => invokeFunction(symTbl, fn, [a, b]), seed, list),
   "range": function (_, stop) {
-    let start = arguments.length <= 2 || arguments[2] === undefined ? 0 : arguments[2];
+    var start = arguments.length <= 2 || arguments[2] === undefined ? 0 : arguments[2];
     return R.range(start, stop);
   },
   "count": (_, x) => R.length(x),
@@ -100,23 +100,23 @@ const symbolTable = {
 };
 
 module.exports = function (symbols, ast) {
-  const symTbl = R.merge(symbolTable, symbols);
+  var symTbl = R.merge(symbolTable, symbols);
   return ast.map(evaluate.bind(null, symTbl));
 };
 
-const isLambda = R.propEq("type", "lambda");
-const isJsFunction = R.compose(R.equals("Function"), R.type);
-const isVector = Array.isArray;
-const isIdentifier = $ => $.type === "identifier";
-const isLazyExp = R.propEq("type", "lazyExp");
+var isLambda = R.propEq("type", "lambda");
+var isJsFunction = R.compose(R.equals("Function"), R.type);
+var isVector = Array.isArray;
+var isIdentifier = $ => $.type === "identifier";
+var isLazyExp = R.propEq("type", "lazyExp");
 
 function evaluate(symbolTable, ast) {
-  const isString = $ => typeof $ === "string";
-  const isNumber = $ => typeof $ === "number";
-  const isBoolean = $ => typeof $ === "boolean";
-  const isList = $ => $.type === "list";
-  const isNil = R.equals(null);
-  const isShortLambda = $ => $.type === "slambda";
+  var isString = $ => typeof $ === "string";
+  var isNumber = $ => typeof $ === "number";
+  var isBoolean = $ => typeof $ === "boolean";
+  var isList = $ => $.type === "list";
+  var isNil = R.equals(null);
+  var isShortLambda = $ => $.type === "slambda";
 
   return R.cond([[isNil, R.always(null)], [isString, R.identity], [isNumber, R.identity], [isBoolean, R.identity], [isLambda, R.identity], [isJsFunction, R.identity], [isShortLambda, compileSLambda.bind(null, symbolTable)], [isList, evalList.bind(null, symbolTable)], [isVector, R.identity], [isIdentifier, id => lookupIdentifier(symbolTable, id.value)], [R.T, throwError.bind(null, "Unable to evaluate", ast)]])(ast);
 }
@@ -127,7 +127,7 @@ function throwError(msg, arg) {
 }
 
 function toList(args) {
-  const list = args.slice(0);
+  var list = args.slice(0);
   return Object.defineProperty(list, "type", { value: "list", enumerable: false });
 }
 
@@ -136,23 +136,23 @@ function compileSLambda(symbolTable, args) {
 }
 
 function lookupIdentifier(symbolTable, id) {
-  const resolveIfLazy = R.ifElse(isLazyExp, resolveLazyExpression, R.identity);
+  var resolveIfLazy = R.ifElse(isLazyExp, resolveLazyExpression, R.identity);
   return id in symbolTable ? resolveIfLazy(symbolTable[id]) : eval(id);
 }
 
 function evalList(symbolTable, list) {
-  const fn = R.head(list);
-  const args = R.tail(list);
-  const isDefinition = $ => $.value === "def";
-  const isConditional = $ => $.value === "if";
-  const isLambdaDefinition = $ => $.value === "fn";
+  var fn = R.head(list);
+  var args = R.tail(list);
+  var isDefinition = $ => $.value === "def";
+  var isConditional = $ => $.value === "if";
+  var isLambdaDefinition = $ => $.value === "fn";
 
   return R.cond([[isConditional, evalConditional.bind(null, symbolTable, args)], [isDefinition, registerDefinition.bind(null, symbolTable, args)], [isLambdaDefinition, compileLambda.bind(null, symbolTable, args)], [R.T, invokeFunction.bind(null, symbolTable, fn, args)]])(fn);
 }
 
 function compileLambda(symbolTable, args) {
-  const params = R.head(args);
-  const body = R.tail(args);
+  var params = R.head(args);
+  var body = R.tail(args);
   return {
     type: "lambda",
     params: params,
@@ -163,16 +163,16 @@ function compileLambda(symbolTable, args) {
 }
 
 function evalConditional(symbolTable, args) {
-  const ev = evaluate.bind(null, symbolTable);
-  const condition = ev(R.nth(0, args));
-  const then = R.nth(1, args);
-  const otherwise = args.length > 2 ? R.nth(2, args) : null;
+  var ev = evaluate.bind(null, symbolTable);
+  var condition = ev(R.nth(0, args));
+  var then = R.nth(1, args);
+  var otherwise = args.length > 2 ? R.nth(2, args) : null;
   return condition ? ev(then) : ev(otherwise);
 }
 
 function registerDefinition(symbolTable, args) {
-  const name = R.head(args).value;
-  const lazyExp = createLazyExpression(symbolTable, R.last(args));
+  var name = R.head(args).value;
+  var lazyExp = createLazyExpression(symbolTable, R.last(args));
   symbolTable[name] = lazyExp;
 }
 
@@ -181,10 +181,10 @@ function createLazyExpression(symbolTable, ast) {
 }
 
 function resolveLazyExpression(expression) {
-  const isResolved = R.propEq("resolved", true);
-  const getValue = R.path(["value"]);
-  const resolveAndGetValue = exp => {
-    const value = evaluate(expression.symbolTable, expression.ast);
+  var isResolved = R.propEq("resolved", true);
+  var getValue = R.path(["value"]);
+  var resolveAndGetValue = exp => {
+    var value = evaluate(expression.symbolTable, expression.ast);
     exp.value = isLazyExp(value) ? resolveLazyExpression(value) : value;
     exp.resolved = true;
     return value;
@@ -193,28 +193,28 @@ function resolveLazyExpression(expression) {
 }
 
 function invokeFunction(symbolTable, fn, args) {
-  const fun = evaluate(symbolTable, fn);
-  const evaledArgs = () => args.map(evaluate.bind(null, symbolTable));
-  const inSymbolTable = value => R.contains(value, R.values(symbolTable));
-  const notInSymbolTable = R.complement(inSymbolTable);
-  const isPureJSFunction = R.both(notInSymbolTable, isJsFunction);
+  var fun = evaluate(symbolTable, fn);
+  var evaledArgs = () => args.map(evaluate.bind(null, symbolTable));
+  var inSymbolTable = value => R.contains(value, R.values(symbolTable));
+  var notInSymbolTable = R.complement(inSymbolTable);
+  var isPureJSFunction = R.both(notInSymbolTable, isJsFunction);
 
   return R.cond([[isLambda, () => executeLambda(symbolTable, fun, args)], [isPureJSFunction, () => fun.apply(undefined, _toConsumableArray(evaledArgs()))], [isJsFunction, () => fun.apply(null, [symbolTable].concat(evaledArgs()))], [R.T, () => new Error("Error in invokeFunction")]])(fun);
 }
 
 function executeLambda(symbolTable, fn, args) {
-  const createLaxyExp = _ref => {
+  var createLaxyExp = _ref => {
     var _ref2 = _slicedToArray(_ref, 2);
 
-    let name = _ref2[0];
-    let value = _ref2[1];
+    var name = _ref2[0];
+    var value = _ref2[1];
     return [name, createLazyExpression(symbolTable, value)];
   };
-  const namedBindings = destructure(fn.params, args).map(createLaxyExp);
-  const positionalParamNames = R.range(0, args.length).map(R.inc).map(R.concat("%"));
-  const positionalBindings = R.zip(positionalParamNames, args);
-  const allBindings = [["%", R.head(args)], ["%&", args]].concat(positionalBindings).concat(namedBindings);
-  const newSymbolTable = R.mergeAll([fn.symbolTable, symbolTable, R.fromPairs(allBindings)]);
+  var namedBindings = destructure(fn.params, args).map(createLaxyExp);
+  var positionalParamNames = R.range(0, args.length).map(R.inc).map(R.concat("%"));
+  var positionalBindings = R.zip(positionalParamNames, args);
+  var allBindings = [["%", R.head(args)], ["%&", args]].concat(positionalBindings).concat(namedBindings);
+  var newSymbolTable = R.mergeAll([fn.symbolTable, symbolTable, R.fromPairs(allBindings)]);
   return R.last(fn.body.map(evaluate.bind(null, newSymbolTable)));
 }
 
@@ -227,14 +227,14 @@ function destructureIdentifier(identifer, value) {
 }
 
 function destructureVector(names, values) {
-  const isRestArg = R.compose(R.propEq("value", "&"), R.head);
-  const isEmpty = (names, values) => R.isEmpty(names) || R.isEmpty(values);
-  const destructureAndCollect = (names, values) => {
-    const first = destructure(R.head(names), R.head(values));
-    const rest = destructureVector(R.tail(names), R.tail(values));
+  var isRestArg = R.compose(R.propEq("value", "&"), R.head);
+  var isEmpty = (names, values) => R.isEmpty(names) || R.isEmpty(values);
+  var destructureAndCollect = (names, values) => {
+    var first = destructure(R.head(names), R.head(values));
+    var rest = destructureVector(R.tail(names), R.tail(values));
     return first.concat(rest);
   };
-  const collectRestArg = (names, values) => destructure(R.nth(1, names), values);
+  var collectRestArg = (names, values) => destructure(R.nth(1, names), values);
 
   return R.cond([[isEmpty, () => []], [isRestArg, collectRestArg], [R.T, destructureAndCollect]])(names, values);
 }
@@ -244,35 +244,35 @@ function destructureVector(names, values) {
 
 var _slicedToArray = (function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; })();
 
-const R = require("ramda");
+var R = require("ramda");
 
 module.exports = parse;
 
-const startsWith = R.curry((needle, stack) => stack.startsWith(needle));
-const startsWithRoundBracket = startsWith("(");
-const startsWithDoubleQuote = startsWith("\"");
-const startsWithCurlyBrace = startsWith("{");
-const startsWithSquareBracket = startsWith("[");
-const startsWithHashRoundBracket = startsWith("#(");
+var startsWith = R.curry((needle, stack) => stack.startsWith(needle));
+var startsWithRoundBracket = startsWith("(");
+var startsWithDoubleQuote = startsWith("\"");
+var startsWithCurlyBrace = startsWith("{");
+var startsWithSquareBracket = startsWith("[");
+var startsWithHashRoundBracket = startsWith("#(");
 
 function parse(str) {
-  const str1 = dropSpaces(str);
-  const isBoolean = $ => $ === "true" || $ === "false";
-  const startsWithColon = startsWith(":");
-  const startsWithHashBracket = startsWith("#{");
-  const startsWithHashDoubleQuote = startsWith("#\"");
-  const isNil = R.equals("nil");
+  var str1 = dropSpaces(str);
+  var isBoolean = $ => $ === "true" || $ === "false";
+  var startsWithColon = startsWith(":");
+  var startsWithHashBracket = startsWith("#{");
+  var startsWithHashDoubleQuote = startsWith("#\"");
+  var isNil = R.equals("nil");
 
   return R.cond([[startsWithRoundBracket, parseList], [isNumber, parseNumber], [startsWithDoubleQuote, parseString], [startsWithCurlyBrace, parseMap], [isBoolean, parseBoolean], [startsWithHashBracket, parseSet], [startsWithColon, parseSymbol], [startsWithSquareBracket, parseVector], [startsWithHashDoubleQuote, parseRegEx], [startsWithHashRoundBracket, parseShortLambda], [isNil, R.always(null)], [R.T, parseIdentifier]])(str1);
 }
 
-const splitAfterMatchingBracket = R.curry(function (openBracket, closeBracket, str) {
+var splitAfterMatchingBracket = R.curry(function (openBracket, closeBracket, str) {
   function throwError() {
     throw new Error("Unbalanced brackets");
   }
 
   function split(left, right, bracketCount) {
-    const h = R.head(right);
+    var h = R.head(right);
     return R.cond([[() => bracketCount === 0, () => [left, right]], [() => isEmptyString(right), throwError], [() => h === openBracket, () => split(left + h, R.tail(right), bracketCount + 1)], [() => h === closeBracket, () => split(left + h, R.tail(right), bracketCount - 1)], [R.T, () => split(left + h, R.tail(right), bracketCount)]])();
   }
 
@@ -280,12 +280,12 @@ const splitAfterMatchingBracket = R.curry(function (openBracket, closeBracket, s
 });
 
 function parseShortLambda(str) {
-  const list = parseListBody(stripBrackets(R.tail(str)));
+  var list = parseListBody(stripBrackets(R.tail(str)));
   return Object.defineProperty(list, "type", { value: "slambda", enumerable: false });
 }
 
 function parseRegEx(str) {
-  const body = stripBrackets(R.tail(str));
+  var body = stripBrackets(R.tail(str));
   return new RegExp(body);
 }
 
@@ -310,8 +310,8 @@ function parseSetBody(str) {
 
   var _splitAfterNextToken2 = _slicedToArray(_splitAfterNextToken, 2);
 
-  const elem = _splitAfterNextToken2[0];
-  const rest = _splitAfterNextToken2[1];
+  var elem = _splitAfterNextToken2[0];
+  var rest = _splitAfterNextToken2[1];
 
   return parseSetBody(rest).add(parse(elem));
 }
@@ -333,15 +333,15 @@ function parseMapBody(str) {
 
   var _splitAfterNextToken4 = _slicedToArray(_splitAfterNextToken3, 2);
 
-  const key = _splitAfterNextToken4[0];
-  const rest1 = _splitAfterNextToken4[1];
+  var key = _splitAfterNextToken4[0];
+  var rest1 = _splitAfterNextToken4[1];
 
   var _splitAfterNextToken5 = splitAfterNextToken(rest1);
 
   var _splitAfterNextToken6 = _slicedToArray(_splitAfterNextToken5, 2);
 
-  const value = _splitAfterNextToken6[0];
-  const rest2 = _splitAfterNextToken6[1];
+  var value = _splitAfterNextToken6[0];
+  var rest2 = _splitAfterNextToken6[1];
 
   return setMap(parseMapBody(rest2), parse(key), parse(value));
 }
@@ -356,12 +356,12 @@ function parseString(str) {
 }
 
 function parseList(str) {
-  const list = parseListBody(stripBrackets(str));
+  var list = parseListBody(stripBrackets(str));
   return Object.defineProperty(list, "type", { value: "list", enumerable: false });
 }
 
 function parseVector(str) {
-  const vector = parseListBody(stripBrackets(str));
+  var vector = parseListBody(stripBrackets(str));
   return Object.defineProperty(vector, "type", { value: "vector", enumerable: false });
 }
 
@@ -370,16 +370,16 @@ function stripBrackets(str) {
 }
 
 function splitAfterNextToken(str) {
-  const str1 = dropSpaces(str);
-  const dropHeadAndAppendSplit = R.curry((openB, closeB, str) => {
-    const h = R.head(str);
+  var str1 = dropSpaces(str);
+  var dropHeadAndAppendSplit = R.curry((openB, closeB, str) => {
+    var h = R.head(str);
 
     var _splitAfterMatchingBr = splitAfterMatchingBracket(openB, closeB, R.drop(1, str));
 
     var _splitAfterMatchingBr2 = _slicedToArray(_splitAfterMatchingBr, 2);
 
-    const left = _splitAfterMatchingBr2[0];
-    const right = _splitAfterMatchingBr2[1];
+    var left = _splitAfterMatchingBr2[0];
+    var right = _splitAfterMatchingBr2[1];
 
     return [h + left, right];
   });
@@ -389,8 +389,8 @@ function splitAfterNextToken(str) {
 
 function splitAfterQuoteEnd(str) {
   function split(left, right) {
-    const curr = R.head(right);
-    const prev = R.last(left);
+    var curr = R.head(right);
+    var prev = R.last(left);
     return R.cond([[() => curr === `"` && prev !== "\\", () => [left + curr, R.tail(right)]], [R.T, () => split(left + curr, R.tail(right))]])();
   }
 
@@ -406,8 +406,8 @@ function parseListBody(str) {
 
   var _splitAfterNextToken8 = _slicedToArray(_splitAfterNextToken7, 2);
 
-  const elem = _splitAfterNextToken8[0];
-  const rest = _splitAfterNextToken8[1];
+  var elem = _splitAfterNextToken8[0];
+  var rest = _splitAfterNextToken8[1];
 
   return [parse(elem)].concat(parseListBody(rest));
 }
@@ -425,7 +425,7 @@ function splitAt(at, str) {
 }
 
 function splitOnNextSpace(str) {
-  const index = str.indexOf(" ");
+  var index = str.indexOf(" ");
 
   if (index !== -1) {
     return splitAt(index, str);
